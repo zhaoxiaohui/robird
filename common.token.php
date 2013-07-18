885c0a49f6f74e019fa27a359a332f6fdbf62a85
zhaoxiaohui <fire.guy@163.com>
2013-07-18 10:59:45 +0800

    测试回复

---
+++ common.token.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on 2013-7-18
+ *
+ * @author firebird
+ * @description 全局token设置
+ */
+ define('ZHHELP', '帮助');
+ define('ENHELP', 'help');
+?>
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 include "Snoopy.class.php";
 include "Robird.php";
-include "Simi.class.php";
+include "common.token.php";
 
 $rbird = new Robird();
+
 if($rbird->checkSignature()){
 	$recieve = $rbird->parseData();
 	
-	/*$name = "firebird";
-	$sim = new Simsimi(array('sid'=>$name,'datapath'=>'sim_'));
-	$ready = $sim->init();*/
 	
-	//$rbird->sendText($recieve['FromUserName'],$recieve['ToUserName'],"text","y");
-	
-	//$reply = $sim->talk($recieve['Content']);
-	$simi = new Simi();
+	/*$simi = new Simi();
 	
 	$reply = $simi->request($recieve['Content']);
 	if($reply['result'] == 100 )
 		$reply = $reply['response'];
 	else $reply = "小火鸟";
-	$rbird->sendText($recieve['FromUserName'],$recieve['ToUserName'],"text",$reply);
+	$rbird->sendText($recieve['FromUserName'],$recieve['ToUserName'],"text",$reply);*/
+	$content = $recieve['Content'];
+	if($content == ZHHELP || $content == ENHELP){
+		$reply = '1.输入数字可以切换频道';
+		$rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', $reply);
+	}
 }
 ?>
