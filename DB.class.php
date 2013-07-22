5179c9ed898194b1633ada6becb14d04836bc828
zhaoxiaohui <fire.guy@163.com>
2013-07-22 17:29:38 +0800

    更新更新

--- DB.class.php
+++ DB.class.php
@@ -2,10 +2,68 @@
 /*=========================================*\
  * @author zh
  * 数据库相关操作
+ * 单例模式
 \*=========================================*/
 class DB {
 
-    function DB() {
+    private static __instance = null;
+
+    private function __construct(){
+    }
+
+    public static function getInstance(){
+        if(is_null(self::$__instance)){
+            self::$__instance = new self();
+        }
+        return self::$__instance;
+    }
+
+    /*==================================*\
+     *function: 检查用户是否已经注册
+     *return: yes or false
+    \*==================================*/
+    public function checkIsSignUp($username){
+        return false;
+    }
+    /*=================================*\
+     *function: 获得用户的房间列表
+     *return: list
+    \*=================================*/
+    public function getRooms($username){
+        return null;
+    }
+    /*=================================*\
+     *function: 获得用户创建的房间数
+     *return: int
+    \*=================================*/
+    public function getRoomNum($username){
+        return 0;
+    }
+    /*=================================*\
+     *function: 用户房间增加1
+    \*=================================*/
+    public function addOneRoom($username){
+    }
+    /*=================================*\
+     *function: 用户房间减少1
+    \*=================================*/
+    public function removeOneRoom($username){
+    }
+    /*=================================*\
+     *function: 用户注册
+    \*=================================*/
+    public function signUp($username, $usernickname, $password){
+    }
+    /*================================*\
+     *function: 用户注销，删除记录
+    \*================================*/
+    public function signOut($username){
+    }
+    /*================================*\
+     *function: 用户充值，用户金币用于
+     *          广播
+    \*================================*/
+    public function addCash($username){
     }
 }
 ?>
--- RedisOp.class.php
+++ RedisOp.class.php
@@ -2,14 +2,14 @@ i<?php
 /*=======================================*\
  * @author zh
  * Redis相关操作
- * 
+ * 单例模式
 \*=======================================*/
 class RedisOp {
 
 	/* private vars */
 	private static $redis = null;
 	private static $redisOp = null;
-    public function __construct() {
+    private function __construct() {
     	self::$redis = new Redis();
     	self::$redis->connect('127.0.0.1');
     }
@@ -19,6 +19,14 @@ class RedisOp {
             self::$redisOp = new self();
         return self::$redisOp;
     }
+    /*======================================*\
+     * 用户改变房间
+     * 不需要权限认证
+     * 查看用户当前房间，如果没有设置房间或者
+     * 当前房间和欲进入房间不一样，则更新记录
+     * 更新的记录主要有房间following
+     * 和用户房间ID号
+    \*======================================*/
     public function changeRoom($username, $roomid){
     	
         //self::$redis = new Redis();
@@ -30,6 +38,26 @@ class RedisOp {
     		self::$redis->set("username:$username:roomid", $roomid);
     		self::$redis->sAdd("roomid:$roomid:following", $username);
     	}
+    }
+    
+    /*=======================================*\
+     * 创建房间
+     * 需要权限认证
+     * 首先检查权限，如果用户已经注册则可以
+     * 创建房间，每个人限制创建房间为5个
+    \*=======================================*/
+
+    public function createRoom($username, $roomid){
+        //首先检查是否已经注册 注册俩表regist:username
+        if(!self::$redis->sIsMember("regist:username", $username)){
+            return ERROR_NO_AUTH;    
+        }
+        //检查用户房间数是不是超过限定
+        if(self::$redis->lSize("username:$username:rooms") > ROOM_ONE_PERSON){
+            return ERROR_ROOM_EXCEED;
+        }
+        //OK 检查完转到创建房间页面
+        return OP_OK;
     }
 }
 ?>
