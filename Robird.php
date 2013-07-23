c879eec1bcffad40b4d82500e6b3401d669419a1
zhaoxiaohui <fire.guy@163.com>
2013-07-23 12:19:41 +0800

    xiugai

--- Robird.php
+++ Robird.php
@@ -6,6 +6,7 @@ define("METHOD", "redis");
 
 class Robird
 {
+    public $postStr = array();
 	// 构造函数
 	public function __construct(){
 		// 读取cookie
@@ -107,26 +108,115 @@ class Robird
 			$this->login();
 		}
 		return $result;
-	}
-	
-	/**
-	 * 被动发送链接
-	 */
-	public function sendUrl($fromUsername, $toUsername, $msgType, $til, $des, $u){
-        $createTime = time();
-		$resultStr = "<xml>
-					<ToUserName><![CDATA[$fromUsername]]></ToUserName>
-					<FromUserName><![CDATA[$toUsername]]></FromUserName>
-					<CreateTime>$createTime</CreateTime>
-					<MsgType><![CDATA[$msgType]]></MsgType>
-					<Title><![CDATA[$til]]></Title>
-					<Description><![CDATA[$des]]></Description>
-					<Url><![CDATA[http://www.baidu.com]]></Url>
-					<FuncFlag>0</FuncFlag>
-					</xml>";
-		//$resultStr = sprintf($resultStr, time());    
-        echo $resultStr;
-	}
+    }
+    public static function xmlSafeStr($str)
+    {   
+        return '<![CDATA['.preg_replace("/[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]/",'',$str).']]>';   
+    } 
+    /**
+    * 数据XML编码
+    * @param mixed $data 数据
+    * @return string
+    */
+    public static function data_to_xml($data) {
+        $xml = '';
+        foreach ($data as $key => $val) {
+            is_numeric($key) && $key = "item id=\"$key\"";
+            $xml    .=  "<$key>";
+            $xml    .=  ( is_array($val) || is_object($val)) ? self::data_to_xml($val)  : self::xmlSafeStr($val);
+            list($key, ) = explode(' ', $key);
+            $xml    .=  "</$key>";
+        }
+        return $xml;
+    }   
+
+    /**
+    * XML编码
+    * @param mixed $data 数据
+    * @param string $root 根节点名
+    * @param string $item 数字索引的子节点名
+    * @param string $attr 根节点属性
+    * @param string $id   数字索引子节点key转换的属性名
+    * @param string $encoding 数据编码
+    * @return string
+    */
+    public function xml_encode($data, $root='xml', $item='item', $attr='', $id='id', $encoding='utf-8') {
+        if(is_array($attr)){
+            $_attr = array();
+            foreach ($attr as $key => $value) {
+                $_attr[] = "{$key}=\"{$value}\"";
+            }
+            $attr = implode(' ', $_attr);
+        }
+        $attr   = trim($attr);
+        $attr   = empty($attr) ? '' : " {$attr}";
+        $xml   = "<{$root}{$attr}>";
+        $xml   .= self::data_to_xml($data, $item, $id);
+        $xml   .= "</{$root}>";
+        return $xml;
+    }
+    /**
+     * 回复消息
+     */
+    public function reply($msg){
+        $xmldata = $this->xml_encode($msg);
+        echo $xmldata;
+    }
+    public function setFromUsername($fromUsername){
+        $this->postStr['FromUserName'] = $fromUsername;
+    }
+    public function setToUsername($toUsername){
+        $this->postStr['ToUserName'] = $toUsername;    
+    }
+    public function setCreateTime(){
+        $this->postStr['CreateTime'] = time();
+    }
+    public function setMsgType($msgType){
+        $this->postStr['MsgType'] = $msgType;
+    }
+    public function setFuncFlag($funcFlag = 0){
+        $this->postStr['FuncFlag'] = $funcFlag;
+    }
+    /**
+    * 设置回复图文
+    * @param array $newsData 
+    * 数组结构:
+    *  array(
+    *   [0]=>array(
+    *       'Title'=>'msg title',
+    *       'Description'=>'summary text',
+    *       'PicUrl'=>'http://www.domain.com/1.jpg',
+    *       'Url'=>'http://www.domain.com/1.html'
+    *   ),
+    *   [1]=>....
+    *  )
+    */
+    public function sendNews($newsData=array())
+    {
+
+        $count = count($newsData);
+
+        $msg = array(
+            'ToUserName' => $this->postStr['FromUserName'],
+            'FromUserName'=>$this->postStr['ToUserName'],
+            'MsgType'=>'news',
+            'CreateTime'=>time(),
+            'ArticleCount'=>$count,
+            'Articles'=>$newsData,
+            'FuncFlag'=>0
+        );
+        $this->reply($msg);
+    }
+    /**
+     * 返回原文消息
+     */
+    public function sendOrigin($fromUsername, $toUsername){
+        $this->setToUserName($fromUsername);
+        $this->setFromUserName($toUsername);
+        $this->setCreateTime();
+        $this->setFuncFlag();
+        $this->reply($this->postStr);
+    }
 	/**
 	 * 被动发送内容
 	 * @param  [type] $fromUsername [description]
@@ -137,17 +227,14 @@ class Robird
 	 */
 	public function sendText($fromUsername,$toUsername,$msgType,$content)
 	{
-        $createTime = time();
-		$resultStr = "<xml>
-					<ToUserName><![CDATA[$fromUsername]]></ToUserName>
-					<FromUserName><![CDATA[$toUsername]]></FromUserName>
-					<CreateTime>$createTime</CreateTime>
-					<MsgType><![CDATA[$msgType]]></MsgType>
-					<Content><![CDATA[$content]]></Content>
-					<FuncFlag>0</FuncFlag>
-					</xml>";  
-		//$resultStr = sprintf($resultStr, time());
-		echo $resultStr;
+        $this->setToUsername($fromUsername);
+        $this->setFromUsername($toUsername);
+        $this->setCreateTime();
+        $this->setMsgType($msgType);
+        $this->postStr['Content'] = $content;
+        $this->setFuncFlag();
+
+        $this->reply($this->postStr);
 	}
 
 	/**
@@ -155,15 +242,14 @@ class Robird
 	 * @return [type] [description]
 	 */
 	public function parseData(){
-		$return = array();
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-		if (!empty($postStr)){
-			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-			$postObj = json_encode($postObj);
-			$postObj = json_decode($postObj,1);
-			return $postObj;
+		//$return = array();
+		$this->postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+		if (!empty($this->postStr)){
+			$this->postStr = simplexml_load_string($this->postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+			$this->postStr = json_encode($this->postStr);
+			$this->postStr = json_decode($this->postStr,1);
 		}else {
-			return $return;
+			return $this->postStr = array();
 		}
 	}
 
--- index.php
+++ index.php
@@ -16,31 +16,48 @@ $redisOp = RedisOp::getInstance();
 //$redisOp = new RedisOp();
 
 if($rbird->checkSignature()){
-	$recieve = $rbird->parseData();
+	$rbird->parseData();
 	
 	//获得命令
-   	$parseResult = json_decode($parse->parseContent($recieve['Content']));
-    //$rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', $parseResult->type);
-	try{
+   	$parseResult = json_decode($parse->parseContent($rbird->postStr['Content']));
+    //$pic = $rbird->postStr['PicUrl'];
+    
+    $news = array(
+        [0]=>array(
+            'Title'=>'哈哈',
+            'Description'=>'你好',
+            'PicUrl'=>'http://219.142.86.69/robird/rong.JPG',
+            'Url'=>'http://219.142.86.69/robird/signUp.php'
+        ),
+        [1]=>array(
+            'Title'=>'哈哈',
+            'Description'=>'你好',
+            'PicUrl'=>'http://219.142.86.69/robird/rong.JPG',
+            'Url'=>'http://219.142.86.69/robird/signUp.php'
+        )
+    );
+    $rbird->sendNews($news);
+    /*
+    try{
         switch($parseResult->type){
 		    //改变房间
 		    case CHRM:
-			    $redisOp->changeRoom($recieve['FromUserName'], $parseResult->con);
-			    $rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', '更新房间成功 现在房间号'.$parseResult->con);
+			    $redisOp->changeRoom($rbird->postStr['FromUserName'], $parseResult->con);
+			    $rbird->sendText($rbird->postStr['FromUserName'], $rbird->postStr['ToUserName'], 'text', '更新房间成功 现在房间号'.$parseResult->con);
 		    	break;
 		    //创建房间
 		    case CRRM:
-		    	$createRsult = $redisOp->createRoom($recieve['FromUserName']);
+		    	$createRsult = $redisOp->createRoom($rbird->postStr['FromUserName']);
 		    	switch($createRsult){
 		    		case ERROR_NO_AUTH:
 		    			//$rbird->sendUrl($recieve['FromUserName'], $recieve['ToUserName'], 'link', '创建房间', '创建房间', SIGN_UP);
-		    			$rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', '<a href="'.SIGN_UP.'">没有注册，无法创建房间，请先创建房间</a>');
+		    			$rbird->sendText($rbird->postStr['FromUserName'], $rbird->postStr['ToUserName'], 'text', '<a href="'.SIGN_UP.'">没有注册，无法创建房间，请先创建房间</a>');
 		    			break;
 		    		case ERROR_ROOM_EXCEED:
-		    			$rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', '房间已满，请删除不需要的房间');
+		    			$rbird->sendText($rbird->postStr['FromUserName'], $rbird->postStr['ToUserName'], 'text', '房间已满，请删除不需要的房间');
 		    			break;
 		    		case OP_OK:
-		    			$rbird->sendUrl($recieve['FromUserName'], $recieve['ToUserName'], 'link', '创建房间', '创建房间', SIGN_UP);
+		    			$rbird->sendText($rbird->postStr['FromUserName'], $rbird->postStr['ToUserName'], 'text', '创建房间');
 		    			break;	
 		    	}
 		    	break;
@@ -57,6 +74,7 @@ if($rbird->checkSignature()){
     }catch(Exception $e){
         $rbird->sendText($recieve['FromUserName'], $recieve['ToUserName'], 'text', $e->getMessage());
     }
+    */
 	/*if($content == '帮助' || $content == 'help'){
 		$reply = '你好';
         //$reply = urldecode($reply);
