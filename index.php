b66249dffec1822e94db93ed15549b9e8a37da2d
zhaoxiaohui <fire.guy@163.com>
2013-07-23 23:25:31 +0800

    test

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if($rbird->checkSignature()){
 			    case SEARCH_MUSIC:
 			    	//$rbird->sendText('sorry啊，木有找到~~~换首歌呗^_^');
 			    	$getContent = new GetContent();
-			    	$musicinfo = json_decode($getContent->getContent($parseResult->con), true);
+			    	//$musicinfo = json_decode($getContent->getContent($parseResult->con), true);
 			    	$rbird->sendText('sorry啊，木有找到~~~换首歌呗^_^');
 			    	/*if($musicinfo['music']['hqmusicurl'] == ""){
 			    		$rbird->sendText('sorry啊，木有找到~~~换首歌呗^_^');
