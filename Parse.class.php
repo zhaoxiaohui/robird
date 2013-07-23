0bc190996d3d9c75d9e33ba4b32ea55ce65b29f1
zhaoxiaohui <fire.guy@163.com>
2013-07-24 00:24:59 +0800

    发送音乐完成

--- Parse.class.php
+++ Parse.class.php
@@ -29,7 +29,7 @@ class Parse {
     	}else if(preg_match("/^(c|C)+(k|K)+$/", $content)){  //check all room
     		return json_encode(array("type"=>CKRMA));
     	}else if(substr($content, 0, 6) == '点歌'){
-    		return json_encode(array('type'=>SEARCH_MUSIC, 'con'=>ltrim(substr($content, 6, strlen($content)))));
+    		return json_encode(array('type'=>SEARCH_MUSIC, 'con'=>trim(substr($content, 6, strlen($content)))));
     	}else return json_encode(array("type"=>SDM, "con"=>$content));
     }
     
